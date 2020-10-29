2214795f9998d9e49f9b5458937e860e9c70aba3
E0594047 <SaumenDas@eaton.com>
2020-10-29 12:08:47 +0530

    Initial Commit

---
+++ src/main/java/com/signify/test/logging/level/spring/cloud/discovery/SpringCloudDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.signify.test.logging.level.spring.cloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signify/test/logging/level/spring/cloud/discovery/SpringCloudDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signify.test.logging.level.spring.cloud.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
